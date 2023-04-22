707c7db19e940e04b4fd9049444155ee082b9ca2
夕久 <3090018282@qq.com>
2023-04-22 11:08:18 +0800

    左叶子之和

--- src/com/tch/tree/Solution15.java
+++ src/com/tch/tree/Solution15.java
@@ -1,8 +1,5 @@
 package com.tch.tree;
 
-import java.util.LinkedList;
-import java.util.Queue;
-
 /**
  * @author : tchstart
  * @version v1.0
@@ -12,22 +9,44 @@ import java.util.Queue;
  * 力扣链接：https://leetcode.cn/problems/find-bottom-left-tree-value/
  **/
 public class Solution15 {
-    Queue<TreeNode> queue = new LinkedList<>();
+    int maxDepth = Integer.MIN_VALUE;
+    int result = 0;
     public int findBottomLeftValue(TreeNode root) {
-        if(root == null) return 0;
-        queue.add(root);
-        int res = 0;
-        while(!queue.isEmpty()){
-            int size = queue.size();
-            for(int i = 0 ; i< size ;i ++){
-                TreeNode treeNode = queue.poll();
-                if(i == 0 ){
-                    res = treeNode.val;
-                }
-                if(treeNode.left != null) queue.add(treeNode.left);
-                if(treeNode.right != null) queue.add(treeNode.right);
+        traversal(root,0);
+        return maxDepth;
+    }
+    public void traversal(TreeNode root ,int dept){
+        if(root == null) return;
+        if(root.left == null && root.right ==null) {
+            if (dept > maxDepth) {
+                result = root.val;
+                maxDepth = dept;
             }
         }
-        return res;
+        if(root.left!= null)traversal(root.left,dept+1);
+        if(root.left != null)traversal(root.right,dept+1);
     }
 }
+
+/**
+ *
+ * //迭代法
+ Queue<TreeNode> queue = new LinkedList<>();
+ public int findBottomLeftValue(TreeNode root) {
+ if(root == null) return 0;
+ queue.add(root);
+ int res = 0;
+ while(!queue.isEmpty()){
+ int size = queue.size();
+ for(int i = 0 ; i< size ;i ++){
+ TreeNode treeNode = queue.poll();
+ if(i == 0 ){
+ res = treeNode.val;
+ }
+ if(treeNode.left != null) queue.add(treeNode.left);
+ if(treeNode.right != null) queue.add(treeNode.right);
+ }
+ }
+ return res;
+ }
+ **/
